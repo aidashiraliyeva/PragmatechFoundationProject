1cc97c080b6b4ade461e131d8ed786adc6e096d0
aidashiraliyeva <shiraliyevaida1997@gmail.com>
2021-07-19 16:11:05 +0400

    Js hissesine duzelis olundu

--- Project/Js/gallery.js
+++ Project/Js/gallery.js
@@ -2,12 +2,12 @@ const loadmore = document.querySelector('#loadmore');
     let currentItems = 2;
     loadmore.addEventListener('click', (e) => {
         const elementList = [...document.querySelectorAll('.list .list-element')];
-        for (let i = currentItems; i < currentItems + 2; i++) {
+        for (let i = currentItems; i < currentItems + 1; i++) {
             if (elementList[i]) {
-                elementList[i].style.display = 'block';
+                elementList[i].style.display = 'flex';
             }
         }
-        currentItems += 2;
+        currentItems += 1;
 
         if (currentItems >= elementList.length) {
             event.target.style.display = 'none';
